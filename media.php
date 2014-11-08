94064d65e81508ea03990024af27a5ef55f805e3
jfarese23 <faresejohn@gmail.com>
2014-11-08 12:08:46 -0500

    Create media.php

---
+++ media.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>DoReMiHope</title>
+	<link rel="stylesheet" type="text/css" href="../styles/stylesheet.css">
+</head>
+<body>
+	<div id="header">
+		<a href="./index.php">DOREMIHOPE</a>
+		<?php include './menu.php';?>
+	</div>
+	<h2>Media Links</h2>
+	<div id="figure1"><figure>
+		<img src="" alt="fig1">
+		<figcaption>In this poignant, funny follow-up to his fabled 2006 talk, Sir Ken Robinson makes the case for a radical shift from standardized schools to personalized learning&mdash;creating conditions where kids' natural talents can flourish.</figcaption>
+	</figure></div>
+	<div id="figure2"><figure>
+		<img src="" alt="fig2">
+		<figcaption>Here, Anita Collins explains the fireworks that go off in musicians' brains when they play, and examines some of the long-term positive effects of this mental workout.</figcaption>
+	</figure></div>
+	<div id="figure3"><figure>
+		<img src="" alt="fig3">
+		<figcaption>"Through the eyes of a child you will see the world just as it ought to be"</figcaption>
+	</figure></div>
+	<div id="figure4"><figure>
+		<img src="" alt="fig4">
+		<figcaption>On the 130th anniversary of the founding of Banco Sabadell we wanted to pay homage to our city by means of the campaign "Som Sabadell" (We are Sabadell). This is the flashmob that we arranged as a final culmination with the participation of 100 people from the Vall&egrave; Symphony Orchestra, the Lieder, Amics de l'&Ograve;pera and Coral Belles Arts choirs.</figcaption>
+	</figure></div>
+	<div id="figure5"><figure>
+		<img src="" alt="fig5">
+		<figcaption>We have encouraged children's creativity: face-to-face assignments: the real and the creative house! Read the story in Reomanian here.</figcaption>
+	</figure></div>
+	<div id="figure6"><figure>
+		<img src="" alt="fig6">
+		<figcaption>Ciprian Porumbescu was a Romanian composer born on October 14, 1853, in Sipotele Sucevei, in Bukovina. He composed the celebrated Ballad for violin and orchestra, a beautiful concert embodying the serenity within the tumult of Romanian people's lives. Aomg his operetta and choral works are: Crai nou, Trei culori, Song for the 1st of May, Pe-al nostru steag e scris Unire, and Serenada.</figcaption>
+	</figure></div>
+</body>
+</html>
