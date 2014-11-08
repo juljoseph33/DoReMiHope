177ea9b0caa0e8b00f347f9e9c813ebeace4c8cf
jfarese23 <faresejohn@gmail.com>
2014-11-08 12:07:20 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>DoReMiHope</title>
+	<link rel="stylesheet" type="text/css" href="../styles/stylesheet.css">
+</head>
+<body>
+	<div id="title"><h1>DoReMiHope</hi></div>
+	<div id="menu">
+		<?php include './menu.php';?>
+	</div>
+</body>
+</html>
