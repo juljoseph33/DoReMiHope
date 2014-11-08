00b7b861fda4d8139eef703dcbf33c9229c75b74
jfarese23 <faresejohn@gmail.com>
2014-11-08 12:08:28 -0500

    Create news.php

---
+++ news.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>DoReMiHope</title>
+	<link rel="stylesheet" type="text/css" href="../styles/stylesheet.css">
+</head>
+<body>
+	<div id="header">
+		<a href="./index.php">DOREMIHOPE</a>
+		<?php include './menu.php';?>
+	</div>
+	<h2>Community Forum</h2>
+	<a class="muut" href="https://muut.com/i/DoReMiHope">Your Community Title</a>
+	<script src="//cdn.muut.com/1/moot.min.js"></script>
+</body>
+</html>
