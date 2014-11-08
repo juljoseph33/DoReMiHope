fa7c801e0d3c277ee44c964df62a4279da9dd640
jfarese23 <faresejohn@gmail.com>
2014-11-08 12:09:21 -0500

    Create menu.php

---
+++ menu.php
@@ -0,0 +1,8 @@
+<?php
+echo '<ul>
+			<li><a href="./about.php">ABOUT</a></li>
+			<li><a href="./news.php">NEWS</a></li>
+			<li><a href="./media.php">MEDIA</a></li>
+			<li><a href="./contact.php">CONTACT</a></li>
+		</ul>';
+?>
