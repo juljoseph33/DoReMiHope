1acd177ce93f006d69f054c887bd5d71c91a89c5
chrdauph <chrdauph@gmail.com>
2014-11-09 17:53:16 -0500

    finished (?) formatting about page
    I based this as much as I could on the sketch that we have. It works
    well in my browser at full screen, not sure about you guys'.

--- about.php
+++ about.php
@@ -16,19 +16,21 @@
 		<p id="aboutIntro">
 			<span class="green">DoReMiHope</span> is a not-for-profit corporation whose purpose is to provide arts programs for Romanian children.<br><br>
 			These programs will fill in a need for arts education and cultural exposure in a society that is recovering from its communist past and struggles to survive economically. Through arts, children will learn to enrich their lives and become responsible and sensiblle individuals who will bring happiness and good to the world. The corporation will support training in performing and visual arts, creative writing, and public speaking.
-		</p>
-		<div id="artPic"><img src="" alt="Art"></div>
+		</p><br>
+		<div id="artPic"><img src="Img/artPic.png" alt="Art"></div>
 		<div id="quote">
-			<h4>Fly, fly, fly high,<br>
+			<h4><em>Fly, fly, fly high,<br>
 				up in the sky.<br>
 				Soar, soar,<br>
-				a little bit more.</h4></div>
-		<div id="brainPic"><img src="" alt="BrainFact"></div>
+				a little bit more.</em></h4>
+		</div>
+		<div id="brainPic"><img src="Img/brainPic.jpg" alt="BrainFact"></div>
+		<br><br>
 		<div id="aboutText">
 			<p>
 				<span class="green">Arts educationis not available</span> in the majority of Romanian schools serving less privileged communities and we are actively identifying these schools to address this need. School principals have been very supportive of our initiative and are willing to cooperate in implementing our programs as extracurricular activities. Our pilot classes in acting and public speaking involved 50 children from the elementary and middle school in Sura Mica, the Sibiu County. The classes raised a lot of enthusiasm among all constituencies, from children to parents, to participating teachers and instructors, and to the larger local community. A press release in Tribuna, the main Sibiu journal, announced our programs and also mentioned our involvement in holiday celebrations for the people of Sura Mica and the nearby villages.
 			</p>
-			<div id="groupPic"><img src="" alt="GroupPic"></div>
+			<div id="groupPic"><img src="Img/groupPic.png" alt="GroupPic"></div>
 			<p>
 				<span class="green">Our future programs</span> will continue to enroll children 6 to 14 years of age and will provide instruction in instrumental music, acting, public speaking, and visual arts. The activities will be tailored to each community and coordinated with the local schools and yout bureaus, as well as with the local artist associations. For this purpose, we reached out to several elementary and middle schools in Romania and we built these relationships to successfully launch our programs after the first round of fundraising in 2014. At the same time, we contacted performing artists interested in collaborating with us and willing to participate as instructors for our programs. We see the role of local artists as a dual component of our mission: while engaging in our programs, the artists provide expert insutrction and, at the same time, find a source of income in a profession generally known for its low monetary rewards. By supports local artists, we will furthermore enrich the exposure of the large community to cultural and artistic events.
 			</p>
