19ea70264648b9aa03d739a8d348cca100001530
Julia Joseph <juljoseph33@hotmail.com>
2014-11-08 21:51:23 -0500

    contact page things

--- contact.php
+++ contact.php
@@ -11,20 +11,22 @@
 
 		<div class="menu"><?php include './menu.php';?></div>
 	</div>
-	<h2>Contact Us</h2>
+	<div id="contact">
+	<h2>CONTACT US</h2>
 	<p>
-		Send us an email at info@doremihope.org!
+		Send us an email at <span id="contactemail">info@doremihope.org</span>!
 	</p>
 	<p>
 		You can also find us on Facebook <a href="https://www.facebook.com/Doremihope" target="_blank">here</a>.
-	</p>
+	</p><br>
 	<h3>Email Form</h3>
 	<form name="input">
 		I am interested in donating:<br>
-		<input type="radio" name="donate" value="yes">Yes<br>
-		<input type="radio" name="donate" value="no">No<br>
-		<input type="email" name="email" placeholder="Email" size="50"><br>
-		<textarea name="message" placeholder="Message" rows="10" cols="90"></textarea><br>
+		<input type="radio" id="yes" name="donate" value="yes">Yes
+		<input type="radio" id="no" name="donate" value="no">No<br><br>
+		<input type="email" id="email" name="email" placeholder="Email" size="50"><br><br>
+		<textarea name="message" id="message" placeholder="Message" rows="10" cols="80"></textarea><br>
 		<input type="submit" name="submit" value="Submit">
 	</form>
+	</div>
 </body>
