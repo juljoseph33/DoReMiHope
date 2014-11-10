7cfa13d0e9b00d53b0803fc7923f1d6fa609d703
Julia Joseph <juljoseph33@hotmail.com>
2014-11-10 12:34:57 -0500

    changed links

--- media.php
+++ media.php
@@ -18,13 +18,13 @@
 				<td>
 					<div id="figure1"><figure>
 						<iframe src="https://embed-ssl.ted.com/talks/sir_ken_robinson_bring_on_the_revolution.html" frameborder="0" scrolling="no" webkitAllowFullScreen mozallowfullscreen allowFullScreen alt="fig1"></iframe>
-						<figcaption>In <a href="http://www.ted.com/talks/sir_ken_robinson_bring_on_the_revolution" class="green">this</a> poignant, funny follow-up to his fabled 2006 talk, Sir Ken Robinson makes the case for a radical shift from standardized schools to personalized learning&mdash;creating conditions where kids' natural talents can flourish.</figcaption>
+						<figcaption>In <a target="_blank" href="http://www.ted.com/talks/sir_ken_robinson_bring_on_the_revolution" class="green">this</a> poignant, funny follow-up to his fabled 2006 talk, Sir Ken Robinson makes the case for a radical shift from standardized schools to personalized learning&mdash;creating conditions where kids' natural talents can flourish.</figcaption>
 					</figure></div>
 				</td>
 				<td>
 					<div id="figure2"><figure>
 						<iframe src="//www.youtube.com/embed/R0JKCYZ8hng" frameborder="0" allowfullscreen alt="fig2"></iframe>
-						<figcaption><a href="http://ed.ted.com/lessons/how-playing-an-instrument-benefits-your-brain-anita-collins" class="green">Here</a>, Anita Collins explains the fireworks that go off in musicians' brains when they play, and examines some of the long-term positive effects of this mental workout.</figcaption>
+						<figcaption><a target="_blank" href="http://ed.ted.com/lessons/how-playing-an-instrument-benefits-your-brain-anita-collins" class="green">Here</a>, Anita Collins explains the fireworks that go off in musicians' brains when they play, and examines some of the long-term positive effects of this mental workout.</figcaption>
 					</figure></div>
 				</td>
 				<td>
@@ -38,19 +38,19 @@
 				<td>
 					<div id="figure4"><figure>
 						<iframe src="//www.youtube.com/embed/GBaHPND2QJg" frameborder="0" alt="fig4" allowfullscreen></iframe>
-						<figcaption>On the 130th anniversary of the founding of Banco Sabadell we wanted to pay homage to our city by means of the campaign "Som Sabadell" (We are Sabadell). <a href="https://www.youtube.com/watch?v=GBaHPND2QJg" class="green">This</a> is the flashmob that we arranged as a final culmination with the participation of 100 people from the Vall&egrave; Symphony Orchestra, the Lieder, Amics de l'&Ograve;pera and Coral Belles Arts choirs.</figcaption>
+						<figcaption>On the 130th anniversary of the founding of Banco Sabadell we wanted to pay homage to our city by means of the campaign "Som Sabadell" (We are Sabadell). <a target="_blank" href="https://www.youtube.com/watch?v=GBaHPND2QJg" class="green">This</a> is the flashmob that we arranged as a final culmination with the participation of 100 people from the Vall&egrave; Symphony Orchestra, the Lieder, Amics de l'&Ograve;pera and Coral Belles Arts choirs.</figcaption>
 					</figure></div>
 				</td>
 				<td>
 					<div id="figure5"><figure>
 						<img src="Img/casasec.jpg" alt="fig5">
-						<figcaption>We have encouraged children's creativity: face-to-face assignments: the real and the creative house! Read the story in Reomanian <a href="http://povestiinspirationale.ro/incurajati-originalitatea/" class="green">here</a>.</figcaption>
+						<figcaption>We have encouraged children's creativity: face-to-face assignments: the real and the creative house! Read the story in Reomanian <a target="_blank" href="http://povestiinspirationale.ro/incurajati-originalitatea/" class="green">here</a>.</figcaption>
 					</figure></div>
 				</td>
 				<td>
 					<div id="figure6"><figure>
 						<iframe src="//www.youtube.com/embed/VzBXJB_otlc" frameborder="0" alt="alt6" allowfullscreen></iframe>
-						<figcaption>Ciprian Porumbescu was a Romanian composer born on October 14, 1853, in Sipotele Sucevei, in Bukovina. He composed the <a href="https://www.youtube.com/watch?v=VzBXJB_otlc" class="green">celebrated Ballad</a> for violin and orchestra, a beautiful concert embodying the serenity within the tumult of Romanian people's lives. Aomg his operetta and choral works are: Crai nou, Trei culori, Song for the 1st of May, Pe-al nostru steag e scris Unire, and Serenada.</figcaption>
+						<figcaption>Ciprian Porumbescu was a Romanian composer born on October 14, 1853, in Sipotele Sucevei, in Bukovina. He composed the <a target="_blank" href="https://www.youtube.com/watch?v=VzBXJB_otlc" class="green">celebrated Ballad</a> for violin and orchestra, a beautiful concert embodying the serenity within the tumult of Romanian people's lives. Aomg his operetta and choral works are: Crai nou, Trei culori, Song for the 1st of May, Pe-al nostru steag e scris Unire, and Serenada.</figcaption>
 					</figure></div>
 				</td>
 			</tr>
