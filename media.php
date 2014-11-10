3127ed5edec964b856c9a84507b04e7dba908db0
chrdauph <chrdauph@gmail.com>
2014-11-09 21:47:34 -0500

    formatted media
    Unfortunately when I tried doing it without a <table> nothing would line
    up very well. If this turns out to be a problem I can try to fix it the
    next time around, but for now it should be presentable enough.

--- media.php
+++ media.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>DoReMiHope</title>
+	<title>DoReMiHope Media</title>
 	<link rel="stylesheet" type="text/css" href="styles/stylesheet.css">
 </head>
 <body>
@@ -11,30 +11,50 @@
 
 		<div class="menu"><?php include './menu.php';?></div>
 	</div>
-	<h2>Media Links</h2>
-	<div id="figure1"><figure>
-		<img src="" alt="fig1">
-		<figcaption>In this poignant, funny follow-up to his fabled 2006 talk, Sir Ken Robinson makes the case for a radical shift from standardized schools to personalized learning&mdash;creating conditions where kids' natural talents can flourish.</figcaption>
-	</figure></div>
-	<div id="figure2"><figure>
-		<img src="" alt="fig2">
-		<figcaption>Here, Anita Collins explains the fireworks that go off in musicians' brains when they play, and examines some of the long-term positive effects of this mental workout.</figcaption>
-	</figure></div>
-	<div id="figure3"><figure>
-		<img src="" alt="fig3">
-		<figcaption>"Through the eyes of a child you will see the world just as it ought to be"</figcaption>
-	</figure></div>
-	<div id="figure4"><figure>
-		<img src="" alt="fig4">
-		<figcaption>On the 130th anniversary of the founding of Banco Sabadell we wanted to pay homage to our city by means of the campaign "Som Sabadell" (We are Sabadell). This is the flashmob that we arranged as a final culmination with the participation of 100 people from the Vall&egrave; Symphony Orchestra, the Lieder, Amics de l'&Ograve;pera and Coral Belles Arts choirs.</figcaption>
-	</figure></div>
-	<div id="figure5"><figure>
-		<img src="" alt="fig5">
-		<figcaption>We have encouraged children's creativity: face-to-face assignments: the real and the creative house! Read the story in Reomanian here.</figcaption>
-	</figure></div>
-	<div id="figure6"><figure>
-		<img src="" alt="fig6">
-		<figcaption>Ciprian Porumbescu was a Romanian composer born on October 14, 1853, in Sipotele Sucevei, in Bukovina. He composed the celebrated Ballad for violin and orchestra, a beautiful concert embodying the serenity within the tumult of Romanian people's lives. Aomg his operetta and choral works are: Crai nou, Trei culori, Song for the 1st of May, Pe-al nostru steag e scris Unire, and Serenada.</figcaption>
-	</figure></div>
+	<div id="media">
+		<h2 class="green" id="mediaTitle">Media Links</h2>
+		<table>
+			<tr>
+				<td>
+					<div id="figure1"><figure>
+						<iframe src="https://embed-ssl.ted.com/talks/sir_ken_robinson_bring_on_the_revolution.html" frameborder="0" scrolling="no" webkitAllowFullScreen mozallowfullscreen allowFullScreen alt="fig1"></iframe>
+						<figcaption>In <a href="http://www.ted.com/talks/sir_ken_robinson_bring_on_the_revolution" class="green">this</a> poignant, funny follow-up to his fabled 2006 talk, Sir Ken Robinson makes the case for a radical shift from standardized schools to personalized learning&mdash;creating conditions where kids' natural talents can flourish.</figcaption>
+					</figure></div>
+				</td>
+				<td>
+					<div id="figure2"><figure>
+						<iframe src="//www.youtube.com/embed/R0JKCYZ8hng" frameborder="0" allowfullscreen alt="fig2"></iframe>
+						<figcaption><a href="http://ed.ted.com/lessons/how-playing-an-instrument-benefits-your-brain-anita-collins" class="green">Here</a>, Anita Collins explains the fireworks that go off in musicians' brains when they play, and examines some of the long-term positive effects of this mental workout.</figcaption>
+					</figure></div>
+				</td>
+				<td>
+					<div id="figure3"><figure>
+						<img src="Img/Ioana card copy.png" alt="fig3">
+						<figcaption>"Through the eyes of a child you will see the world just as it ought to be"</figcaption>
+					</figure></div>
+				</td>
+			</tr>
+			<tr>
+				<td>
+					<div id="figure4"><figure>
+						<iframe src="//www.youtube.com/embed/GBaHPND2QJg" frameborder="0" alt="fig4" allowfullscreen></iframe>
+						<figcaption>On the 130th anniversary of the founding of Banco Sabadell we wanted to pay homage to our city by means of the campaign "Som Sabadell" (We are Sabadell). <a href="https://www.youtube.com/watch?v=GBaHPND2QJg" class="green">This</a> is the flashmob that we arranged as a final culmination with the participation of 100 people from the Vall&egrave; Symphony Orchestra, the Lieder, Amics de l'&Ograve;pera and Coral Belles Arts choirs.</figcaption>
+					</figure></div>
+				</td>
+				<td>
+					<div id="figure5"><figure>
+						<img src="Img/houses.jpg" alt="fig5">
+						<figcaption>We have encouraged children's creativity: face-to-face assignments: the real and the creative house! Read the story in Reomanian <a href="http://povestiinspirationale.ro/incurajati-originalitatea/" class="green">here</a>.</figcaption>
+					</figure></div>
+				</td>
+				<td>
+					<div id="figure6"><figure>
+						<iframe src="//www.youtube.com/embed/VzBXJB_otlc" frameborder="0" alt="alt6" allowfullscreen></iframe>
+						<figcaption>Ciprian Porumbescu was a Romanian composer born on October 14, 1853, in Sipotele Sucevei, in Bukovina. He composed the <a href="https://www.youtube.com/watch?v=VzBXJB_otlc" class="green">celebrated Ballad</a> for violin and orchestra, a beautiful concert embodying the serenity within the tumult of Romanian people's lives. Aomg his operetta and choral works are: Crai nou, Trei culori, Song for the 1st of May, Pe-al nostru steag e scris Unire, and Serenada.</figcaption>
+					</figure></div>
+				</td>
+			</tr>
+		</table>
+	</div>
 </body>
 </html>
