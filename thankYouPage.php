941a8e9906f8cc979d93970f5509bf11ce681480
jfarese23 <faresejohn@gmail.com>
2014-11-15 13:52:24 -0500

    Create thankYouPage.php

---
+++ thankYouPage.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>DoReMiHope</title>
+	<link rel="stylesheet" type="text/css" href="./stylesheet.css">
+</head>
+<body>
+	<div id="header">
+		<a href="./index.php"><span id="doremi">DOREMIHOPE</span></a>
+		<img src="Img/logocircle.png">
+
+		<div class="menu"><?php include './menu.php';?></div>
+	</div>
+	<div id="contact">
+	<h2>THANK YOU!</h2>
+	<p>Thanks for your response! We really appreciate your input and will be in contact as soon as possible!</p>
+	<p>
+		Sincerely,<br>
+		DoReMiHope</p>
+</body>
+</html>
