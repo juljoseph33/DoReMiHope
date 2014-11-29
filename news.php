14f3d03675f376f86022068e5fa76457828d51ff
chrdauph <chrdauph@gmail.com>
2014-11-29 17:32:44 -0500

    Ticker
    I added a news ticker to the top of the news page. I couldn't get the
    border-radius to match the rest of the pages, and I also don't
    understand the CSS of the forum enough to get that in the same div, but
    if one of you guys could do that (and also figure out what to put in the
    ticker) that'd be awesome.

--- news.php
+++ news.php
@@ -3,6 +3,7 @@
 <head>
 	<title>DoReMiHope</title>
 	<link rel="stylesheet" type="text/css" href="styles/stylesheet.css">
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
 </head>
 <body>
 	<div id="header">
@@ -10,6 +11,10 @@
 		<img alt="logo" src="Img/logocircle.png">
 
 		<div class="menu"><?php include './menu.php';?></div>
+	</div>
+	<div id="news">
+		<h2>News</h2>
+		<div id="ticker"><?php include 'ticker.html';?></div>
 	</div>
 	<h2>Community Forum</h2>
 	<a class="muut" href="https://muut.com/i/DoReMiHope">Your Community Title</a>
