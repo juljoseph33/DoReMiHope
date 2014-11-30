2971dba49a4a07ff7975b7324fb3f20c4f4ebf87
Julia Joseph <juljoseph33@hotmail.com>
2014-11-30 18:16:44 -0500

    fixed styling for news page

--- news.php
+++ news.php
@@ -7,7 +7,7 @@
 	<script src="JS/backstretch.js"></script>
 	<script src="JS/style.js"></script>
 </head>
-<body id="news" class="repeat">
+<body class="repeat">
 	<div id="header">
 		<a href="./index.php"><span id="doremi">DOREMIHOPE</span></a>
 		<img alt="logo" src="Img/logocircle.png">
@@ -16,14 +16,14 @@
 	</div>
 
 	<div id="news">
-		<h2>News</h2>
-		<div id="ticker"><?php include 'ticker.php';?></div>
-	</div>
+	<h2>News</h2>
+	<div id="ticker"><?php include 'ticker.php';?></div>
 	
 	<h2>Community Forum</h2>
 	<div id="forum">
 	<a class="muut" href="https://muut.com/i/DoReMiHope">Your Community Title</a>
 	<script src="//cdn.muut.com/1/moot.min.js"></script>
+	</div>
 	</div>
 </body>
 </html>
